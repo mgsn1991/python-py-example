924a02bd7fa0f94f7b4ea4e5314e3748dd83b2f9
Marc <marc.schoenwasser@web.de>
2018-05-01 20:27:02 +0200

    new branch

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main(msg):
 //wow a change again, commit issues incoming wow
 //Versioncontrol in IDE
 //muhahaha
+//no longer comments needed V0.1
 
-
+def secondary():
+    print("New function")
 main("Hello World!")
