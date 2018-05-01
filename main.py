a732fffb67e48d98a92ff7d33a023929a8e3ae69
Marc <marc.schoenwasser@web.de>
2018-05-01 20:30:30 +0200

    newest commit

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 def main(msg):
     print(msg)
 
-//wow a change again, commit issues incoming wow
-//Versioncontrol in IDE
-//muhahaha
-//no longer comments needed V0.1
+#wow a change again, commit issues incoming wow
+#Versioncontrol in IDE
+#muhahaha
+#no longer comments needed V0.1
 
 def secondary():
     print("New function")
